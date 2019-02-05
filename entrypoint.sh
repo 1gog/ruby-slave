ebdaae2a90a04c719d5e36528f83f505c4f86323
amirzuev <mirzuev.a.v@gmail.com>
2019-02-05 18:21:53 +0300

    update uid gid fow nss_wrapp and env for container limit memory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+
+# step 1
+# Set current user in nss_wrapper
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
+if [ x"$USER_ID" != x"0" ]; then
+
+    echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> /etc/passwd
+
+fi
+
+# step 2
+CONTAINER_MEMORY_IN_BYTES=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+CONTAINER_MEMORY_IN_MB=$((CONTAINER_MEMORY_IN_BYTES/2**20))
+
 export HOME=/home/jenkins
-export JENKINS_HOME=$HOME
-export JAR="$HOME/agent.jar"
+export JENKINS_HOME=${HOME}
+export JAR="${HOME}/agent.jar"
 if [ ! -e ${HOME}/agent.jar ]; then
 	curl -o ${HOME}/agent.jar --cacert /run/secrets/kubernetes.io/serviceaccount/ca.crt \
 	${JENKINS_URL}/jnlpJars/agent.jar
 fi
 
-if [ ! -z ${JENKINS_SECRET} ] && [ ! -z ${JENKINS_TUNNEL} ]; then 
+if [ ! -z ${JENKINS_SECRET} ] && [ ! -z ${JENKINS_TUNNEL} ]; then
 	echo "Running in JNLP Slave"
 	TUNNEL="-tunnel ${JENKINS_TUNNEL}"
 
-	if [[ ! -z "${JENKINS_URL}" ]]; then 
+	if [[ ! -z "${JENKINS_URL}" ]]; then
 		URL="-url ${JENKINS_URL}"
 	fi
 	exec java ${JAVA_OPTS} -cp ${JAR} hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache ${HOME} -workDir ${JENKINS_HOME} ${JENKINS_SECRET} ${JENKINS_NAME}
